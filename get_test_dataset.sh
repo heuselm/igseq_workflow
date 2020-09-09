15a79363744ef952d4e60f4d0aa3ea99041243ae
heuselm <moritz.heusel@med.lu.se>
2020-09-09 17:23:00 +0200

    add script get_test_dataset.sh

---
+++ get_test_dataset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget --no-check-certificate 'https://www.dropbox.com/s/f446y8pa2fw0n6p/testdataR1_001.fastq.gz?dl=0' -O 00_input/readsR1_001.fastq.gz
+wget --no-check-certificate 'https://www.dropbox.com/s/aygvwelmzxnrqxp/testdataR2_001.fastq.gz?dl=0' -O 00_input/readsR2_001.fastq.gz
+wget --no-check-certificate 'https://www.dropbox.com/s/dc93pp314623yur/barcodes.txt?dl=0' -O 00_input/barcodes.txt
+
